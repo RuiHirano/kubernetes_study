69354383b3f371b37bfc850d5b169c9d77bd0711
Rui Hirano <r.hrn.0930@gmail.com>
2021-07-05 19:11:39 +0900

    update compose file

--- client/client.py
+++ client/client.py
@@ -1,12 +1,12 @@
 import requests
 import time
 
-
 def request():
     while True: 
         time.sleep(1)
-        r = requests.get('http://localhost:5000/test')
+        r = requests.get('http://server:5000/test')
         print(r.text)
 
 if __name__ == "__main__":
+    print("starts")
     request()
